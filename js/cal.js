8991f3c58167e308d433cf8441dd31c0894e26c7
Becca Staple <petite.becca.s@gmail.com>
2020-06-16 12:46:44 +0100

    history function working now

--- js/cal.js
+++ js/cal.js
@@ -2,28 +2,32 @@
     const num1 = document.getElementById("num1");
     const num2 = document.getElementById("num2");
     const myAnswer = document.getElementById("myAnswer");
-    const newOutput = document.createElement("p");
+    
 
 function adding() {
     myAnswer.value = parseInt(num1.value) + parseInt(num2.value);
+    const newOutput = document.createElement("p");
     document.getElementById("output").appendChild(newOutput);
     newOutput.innerText = document.getElementById("num1").value + " + " + document.getElementById("num2").value + " = " + document.getElementById("myAnswer").value;
 }
 
 function subtract() {
     myAnswer.value = parseInt(num1.value) - parseInt(num2.value);
+    const newOutput = document.createElement("p");v
     document.getElementById("output").appendChild(newOutput);
     newOutput.innerText = document.getElementById("num1").value + " - " + document.getElementById("num2").value + " = " + document.getElementById("myAnswer").value;
 }
 
 function divide() {
     myAnswer.value = parseInt(num1.value) / parseInt(num2.value);
+    const newOutput = document.createElement("p");
     document.getElementById("output").appendChild(newOutput);
     newOutput.innerText = document.getElementById("num1").value + " / " + document.getElementById("num2").value + " = " + document.getElementById("myAnswer").value;
 }
 
 function multiply() {
     myAnswer.value = parseInt(num1.value) * parseInt(num2.value);
+    const newOutput = document.createElement("p");
     document.getElementById("output").appendChild(newOutput);
     newOutput.innerText = document.getElementById("num1").value + " * " + document.getElementById("num2").value + " = " + document.getElementById("myAnswer").value;
 }
